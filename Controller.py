0ffd40cc474391b5d6afc79d50449cacba1d082e
Cryptric <gawain.marti@cryptric.ch>
2024-02-14 21:12:05 +0100

    added termination event to gracefully stop producer

--- Controller.py
+++ Controller.py
@@ -1,23 +1,22 @@
-from multiprocessing import Pipe, Process
+from multiprocessing import Pipe, Process, Event
 
 import cv2
-import numpy as np
 
 import Davis346Reader
 
 
-
 def main():
 	consumer_conn, producer_conn = Pipe(False)
-	p: Process = Process(target=Davis346Reader.run, args=(producer_conn, ))
+	termination_event = Event()
+	p: Process = Process(target=Davis346Reader.run, args=(producer_conn, termination_event))
 	p.start()
-	
 	while True:
 		frame = consumer_conn.recv()
 		cv2.imshow("frame", frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
+			termination_event.set()
 			break
-	
+
 
 if __name__ == "__main__":
 	main()
--- Davis346Reader.py
+++ Davis346Reader.py
@@ -1,38 +1,33 @@
-"""DAVIS346 test example.
-
-Author: Yuhuang Hu
-Email : duguyue100@gmail.com
-"""
 from pyaer import libcaer
 from pyaer.davis import DAVIS
 
+from multiprocessing import Event
 from multiprocessing.connection import Connection
 
 
-
-def run(producer_conn: Connection):
+def run(producer_conn: Connection, termination_event: Event):
 	device = DAVIS(noise_filter=True)
 
-	print ("Device ID:", device.device_id)
+	print("Device ID:", device.device_id)
 	if device.device_is_master:
-		print ("Device is master.")
+		print("Device is master.")
 	else:
 		print ("Device is slave.")
-	print ("Device Serial Number:", device.device_serial_number)
-	print ("Device String:", device.device_string)
-	print ("Device USB bus Number:", device.device_usb_bus_number)
-	print ("Device USB device address:", device.device_usb_device_address)
-	print ("Device size X:", device.dvs_size_X)
-	print ("Device size Y:", device.dvs_size_Y)
-	print ("Logic Version:", device.logic_version)
-	print ("Background Activity Filter:", device.dvs_has_background_activity_filter)
+	print("Device Serial Number:", device.device_serial_number)
+	print("Device String:", device.device_string)
+	print("Device USB bus Number:", device.device_usb_bus_number)
+	print("Device USB device address:", device.device_usb_device_address)
+	print("Device size X:", device.dvs_size_X)
+	print("Device size Y:", device.dvs_size_Y)
+	print("Logic Version:", device.logic_version)
+	print("Background Activity Filter:", device.dvs_has_background_activity_filter)
 
 	device.start_data_stream()
 	# setting bias after data stream started
 	device.set_bias_from_json("./davis346_config.json")
 
 	try:
-		while True:
+		while not termination_event.is_set():
 			data = device.get_event()
 			if data is not None:
 				(_, _, _, _, _, frames, _, _) = data
