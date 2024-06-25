1d57d02f83c3b28740a5e3b5d7d69c9dcee4b37f
Cryptric <gawain.marti@cryptric.ch>
2024-06-25 14:53:44 +0200

    adapt corner detection

--- utils/corner-detection-tester/find-corners.py
+++ utils/corner-detection-tester/find-corners.py
@@ -1,15 +1,20 @@
 from functools import partial
 from multiprocessing import Pipe, Event, Process
 
+import cv2
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Slider
+
 import Davis346Reader
-from Params import Y_EDGE, X_EDGE, IMG_SIZE_Y, IMG_SIZE_X, PROCESSING_Y, PROCESSING_SIZE_HEIGHT, PROCESSING_X, \
-	PROCESSING_SIZE_WIDTH, CORNER_MASK_MAX_Y, CORNER_MASK_MIN_X, CORNER_MASK_MAX_X, CORNER_MASK_MIN_Y
+from Params import *
 from utils.ControlUtils import Timer, print_timers
-from utils.FrameUtils import process_frame, remove_distortion, find_board_corners
+from utils.FrameUtils import remove_distortion, find_board_corners, find_corners, process_frame
 from utils.Plotting import pr_cmap
+
+matplotlib.use("TkAgg")
 
 
 def main():
@@ -22,23 +27,33 @@ def main():
 	mask = np.zeros((IMG_SIZE_Y, IMG_SIZE_X))
 	mask[CORNER_MASK_MIN_Y:CORNER_MASK_MAX_Y, CORNER_MASK_MIN_X:CORNER_MASK_MAX_X] = 1
 
-	fig, ax = plt.subplots(nrows=1)
-	img = ax.imshow(np.zeros((IMG_SIZE_Y, IMG_SIZE_X)), cmap="gray", vmin=0, vmax=255)
-	corner_points = ax.scatter([0, 0, 0, 0], [0, 0, 0, 0], label="detected board corners")
-	mask_plt = ax.imshow(mask, cmap=pr_cmap, vmin=0, vmax=1, alpha=0.25)
+	fig, ax = plt.subplots(nrows=5, height_ratios=[6, 1, 1, 1, 1])
+	img = ax[0].imshow(np.zeros((IMG_SIZE_Y, IMG_SIZE_X)), cmap="gray", vmin=0, vmax=255)
+	corners_plt, = ax[0].plot([0], [0], 'ro', markersize=1)
+	board_corner_points = ax[0].scatter([0, 0, 0, 0], [0, 0, 0, 0], label="detected board corners", zorder=100)
+	mask_plt = ax[0].imshow(mask, cmap=pr_cmap, vmin=0, vmax=1, alpha=0.25)
 
-	def update(_, img_plt, corner_points_plt):
+	static_corner_plt, = ax[0].plot([CORNER_BL[0], CORNER_BR[0], CORNER_TR[0], CORNER_TL[0]], [CORNER_BL[1], CORNER_BR[1], CORNER_TR[1], CORNER_TL[1]], 'go')
+
+	slider = Slider(ax[1], 'blockSize', 0, 20, valinit=2, valstep=1)
+	slider2 = Slider(ax[2], 'ksize', 1, 31, valinit=5, valstep=2)
+	slider3 = Slider(ax[3], 'k', 0, 0.1, valinit=0.02)
+	slider4 = Slider(ax[4], 'threshold', 0, 0.4, valinit=0.01)
+
+	def update(_):
 		with Timer("update"):
 			frame, _ = consumer_conn.recv()
+			corners = find_corners(frame, block_size=slider.val, ksize=slider2.val, k=slider3.val, threshold=slider4.val)
 			corner_br, corner_bl, corner_tl, corner_tr = find_board_corners(frame)
-			frame, _ = process_frame(frame)
+			frame = process_frame(frame)
 
-			img_plt.set_array(frame)
-			corner_points.set_offsets([corner_br, corner_bl, corner_tl, corner_tr])
-			return img_plt, corner_points_plt, mask_plt
+			img.set_array(frame)
+			board_corner_points.set_offsets([corner_br, corner_bl, corner_tl, corner_tr])
+			corners_plt.set_xdata(corners[:, 1])
+			corners_plt.set_ydata(corners[:, 0])
+			return img, board_corner_points, mask_plt, corners_plt, static_corner_plt
 
-	update_func = partial(update, img_plt=img, corner_points_plt=corner_points)
-	anim = FuncAnimation(fig, update_func, cache_frame_data=False, interval=0, blit=True)
+	anim = FuncAnimation(fig, update, cache_frame_data=False, interval=0, blit=True)
 
 	plt.show()
 	termination_event.set()
@@ -52,6 +67,7 @@ def main():
 	consumer_conn.close()
 	p.join()
 	print_timers()
+
 
 if __name__ == '__main__':
 	main()
