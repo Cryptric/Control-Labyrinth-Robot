b4c0cd129807dc278bf86cf3b6f22d1c9fa7d6d3
Cryptric <gawain.marti@cryptric.ch>
2024-06-25 14:55:06 +0200

    zoom, argmax detection, better ui

--- utils/nn-tester/image-param-test.py
+++ utils/nn-tester/image-param-test.py
@@ -1,19 +1,24 @@
 from functools import partial
 from multiprocessing import Pipe, Event, Process
 
+import cv2
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from matplotlib import patches
 from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Slider
 import torchvision.transforms.functional as TF
 import CueNetV2
 import Davis346Reader
 from CueNetV2 import device
-from Params import Y_EDGE, X_EDGE, IMG_SIZE_Y, IMG_SIZE_X, PROCESSING_Y, PROCESSING_SIZE_HEIGHT, PROCESSING_X, \
-	PROCESSING_SIZE_WIDTH
+from Params import *
+from utils.ControlUtils import find_center, find_center2
 from utils.FrameUtils import remove_distortion
+from utils.Plotting import pr_cmap
 
+matplotlib.use("TkAgg")
 
 
 def main():
@@ -27,33 +32,47 @@ def main():
 	p.start()
 	producer_conn.close()
 
-	fig, ax = plt.subplots(nrows=4)
+	fig, ax = plt.subplots(nrows=4, height_ratios=[5, 5, 1, 1])
 	slider = Slider(ax[2], 'Bias', 0, 255, valinit=0, valstep=1)
-	slider2 = Slider(ax[3], 'Gain', 0.0, 5.0, valinit=1.0)
+	slider2 = Slider(ax[3], 'Gain', 0.0, 8.0, valinit=1.0)
 
 	img = ax[0].imshow(np.zeros((IMG_SIZE_Y, IMG_SIZE_X)), cmap="gray", vmin=0, vmax=255)
-	pos_heatmap = ax[1].imshow(np.zeros((IMG_SIZE_Y, IMG_SIZE_X)), cmap='inferno', alpha=1)
+	pos_heatmap_overlay = ax[0].imshow(np.zeros((IMG_SIZE_Y, IMG_SIZE_X - 2)), cmap=pr_cmap, alpha=1)
+	pos_heatmap = ax[1].imshow(np.zeros((IMG_SIZE_Y, IMG_SIZE_X - 2)), cmap='inferno', alpha=1)
+
+	pos_patch = plt.Circle((0, 0), 7, fill=False, color='purple', zorder=101)
+	pos_plt = ax[0].add_patch(pos_patch)
+
+	# processing_section_marker = patches.Rectangle((PROCESSING_X, PROCESSING_Y), PROCESSING_SIZE_WIDTH, PROCESSING_SIZE_HEIGHT, linewidth=1, edgecolor='r', facecolor='none')
+	# processing_region = ax[0].add_patch(processing_section_marker)
 
 	def update(_, img_plt, pos_heatmap_plt):
 		def process_frame(pframe):
 			pframe = remove_distortion(pframe)
-			# frame += 96
-			# frame[frame < 96] = 255
-			pframe = np.clip(pframe.astype(np.uint16) * slider2.val + slider.val, 0, 255)
-			return pframe, torch.squeeze(TF.to_tensor(pframe[PROCESSING_Y:PROCESSING_Y + PROCESSING_SIZE_HEIGHT, PROCESSING_X:PROCESSING_X + PROCESSING_SIZE_WIDTH].astype("float32") / 255).to(device))
+			#pframe = cv2.equalizeHist(pframe)
+			pframe = np.clip(pframe.astype(np.uint16) * slider2.val + slider.val, 0, 255).astype(np.uint8)
+			return pframe, torch.squeeze(TF.to_tensor(pframe.astype("float32") / 255).to(device))
 
 		frame, _ = consumer_conn.recv()
 		frame, new_frame = process_frame(frame)
 		if len(frame_buffer) == 3:
-			heatmap = cue_net.calc_position_heatmap(new_frame, frame_buffer.pop(0), frame_buffer[0])
-			heatmap = np.pad(heatmap[0], ((Y_EDGE // 2, Y_EDGE // 2), (X_EDGE // 2, X_EDGE // 2)))
+			heatmap = cue_net.calc_position_heatmap(new_frame, frame_buffer.pop(0), frame_buffer[0])[0]
+			x, y = find_center2(heatmap)
+			print(f"Found ball at x={x}, y={y}")
+			# heatmap = np.pad(heatmap[0], ((Y_EDGE // 2, Y_EDGE // 2), (X_EDGE // 2, X_EDGE // 2)))
 			img_plt.set_array(frame)
+			pos_heatmap_overlay.set_array(heatmap)
 			pos_heatmap_plt.set_array(heatmap)
 			vmin, vmax = heatmap.min(), heatmap.max()
+			pos_heatmap_overlay.set_clim(vmin, vmax)
 			pos_heatmap_plt.set_clim(vmin=vmin, vmax=vmax)
 
 		frame_buffer.append(new_frame)
-		return img_plt, pos_heatmap_plt
+
+		argmax_pos = np.unravel_index(frame.argmax(), frame.shape)
+		pos_plt.set_center(argmax_pos[::-1])
+
+		return img_plt, pos_heatmap_plt, pos_heatmap_overlay, pos_plt
 
 	update_func = partial(update, img_plt=img, pos_heatmap_plt=pos_heatmap)
 	anim = FuncAnimation(fig, update_func, cache_frame_data=False, interval=0, blit=True)
