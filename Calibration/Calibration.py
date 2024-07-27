calibration overflow bugfix

Calibration/Calibration.py
@@ -4,34 +4,14 @@ from sklearn.linear_model import LinearRegression
 
 
 def main():
-	data_files_x = [
-		"DataX3.csv",
-		"DataX4.csv",
-		"DataX5.csv",
-		"DataX6.csv"
-	]
 
-	data_files_y = [
-		"DataY3.csv",
-		"DataY4.csv",
-		"DataY5.csv",
-		"DataY6.csv"
-	]
+	data_x = np.genfromtxt("Calibration/DataX.csv", skip_header=1, delimiter=",")
 
-	data_x = []
-	for f in data_files_x:
-		data_x.append(np.genfromtxt(f, skip_header=1, delimiter=","))
+	data_y = np.genfromtxt("Calibration/DataY.csv", skip_header=1, delimiter=",")
+	data_y[:, 1:3] *= -1
 
-	data_y = []
-	for f in data_files_y:
-		data_y.append(np.genfromtxt(f, skip_header=1, delimiter=","))
-		data_y[-1][:, 1] *= -1
-
-	data_x_stack = np.vstack(data_x)
-	data_y_stack = np.vstack(data_y)
-
-	lr_x = LinearRegression(fit_intercept=True).fit(np.expand_dims(data_x_stack[:, 0], 1), data_x_stack[:, 1])
-	lr_y = LinearRegression(fit_intercept=True).fit(np.expand_dims(data_y_stack[:, 0], 1), data_y_stack[:, 1])
+	lr_x = LinearRegression(fit_intercept=True).fit(np.expand_dims(np.tile(data_x[:, 0], 2), axis=0).T, data_x[:, 1:3].reshape(42, order='F'))
+	lr_y = LinearRegression(fit_intercept=True).fit(np.expand_dims(np.tile(data_y[:, 0], 2), axis=0).T, data_y[:, 1:3].reshape(42, order='F'))
 
 	coef_x = lr_x.coef_
 	coef_y = lr_y.coef_
@@ -44,18 +24,19 @@ def main():
 	fig, axes = plt.subplots(nrows=2)
 
 	for data, coef, bias, ax in zip([data_x, data_y], [coef_x, coef_y], [bias_x, bias_y], axes):
-		for i, d in enumerate(data):
-			ax.plot(d[:, 0], d[:, 1], label=f"Measurement {i + 1}", linestyle="-.")
-		ax.plot(data[0][:, 0], data[0][:, 0] * coef[0] + bias, label="Fitted Line", linewidth="2")
-		ax.set_ylabel("Board tilt [°]")
+		ax.plot(data[:, 0], data[:, 1], label=f"Measurement +10° to -10°", linestyle="-.")
+		ax.plot(data[:, 0], data[:, 2], label=f"Measurement -10° to +10°", linestyle="-.")
+
+		ax.plot(data[:, 0], data[:, 0] * coef[0] + bias, label="Fitted Line", linewidth="2")
+		ax.set_ylabel("Platform angle [°]")
 		ax.set_xlabel("Servo angle [°]")
 		ax.grid()
 		ax.legend()
 
 	axes[0].set_title("X-Axis")
 	axes[1].set_title("Y-Axis")
-	axes[0].set_ylim([-9.9, 9.9])
-	axes[1].set_ylim([-9.9, 9.9])
+	axes[0].set_ylim([-2.2, 2.2])
+	axes[1].set_ylim([-2.2, 2.2])
 
 	# backlash_x = {}
 	# for servo_angle in data_x[0][:, 0]:
@@ -75,8 +56,8 @@ def main():
 	# 	backlash_y[servo_angle] = s / 3
 	# print(backlash_y)
 
-	# import tikzplotlib
-	# tikzplotlib.save("lin-servo-platform.tex")
+	import tikzplotlib
+	tikzplotlib.save("PlotData/lin-servo-platform.tex")
 
 	plt.show()
 